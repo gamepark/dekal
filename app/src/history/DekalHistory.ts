29506f6369d6569c3f0e7f49bcc5426a57b0e6c1
Patrick Beaume <p.beaume@gmail.com>
2025-04-06 13:42:32 +0200

    vertical center messages

--- app/src/history/DekalHistory.ts
+++ app/src/history/DekalHistory.ts
@@ -1,4 +1,5 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { LocationType } from '@gamepark/dekal/material/LocationType'
 import { MaterialType } from '@gamepark/dekal/material/MaterialType'
 import { RuleId } from '@gamepark/dekal/rules/RuleId'
@@ -14,25 +15,33 @@ import { ScoreLog } from './components/ScoreLog'
 export class DekalHistory implements LogDescription {
   getMovePlayedLogDescription(move: MaterialMove, context: MoveComponentContext) {
       if (context.game.rule?.id === RuleId.ChooseCard && isMoveItemType(MaterialType.Card)(move) && move.location.rotation && move.location.type === LocationType.DropArea) {
-        return { Component: ChooseCardLog, player: move.location.player }
+        return { Component: ChooseCardLog, player: move.location.player, css: centerCss }
       }
 
       if (context.game.rule?.id === RuleId.RevealCard && isMoveItemType(MaterialType.Card)(move) && !move.location.rotation) {
-        return { Component: RevealedCardLog, player: move.location.player }
+        return { Component: RevealedCardLog, player: move.location.player, css: centerCss }
       }
 
       if (context.game.rule?.id === RuleId.ChooseRevealedCard && isMoveItemType(MaterialType.Card)(move) && isOutside(move.location as Location)) {
-        return { Component: ChooseRevealedCardLog, player: context.game.rule.player }
+        return { Component: ChooseRevealedCardLog, player: context.game.rule.player, css: centerCss }
       }
 
       if (isMoveItemType(MaterialType.FirstPlayer)(move)) {
-        return { Component: ChangeFirstPlayerLog}
+        return { Component: ChangeFirstPlayerLog, css: centerCss}
       }
 
       if (isEndGame(move)) {
-        return { Component: ScoreLog }
+        return { Component: ScoreLog, css: centerCss }
       }
 
       return
     }
 }
+
+const centerCss = css`
+  > div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`
